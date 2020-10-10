1159c8d3e7d0232e98536e4622ed9f832ab6d8a3
lgstvo <lgustavo2001@gmail.com>
2020-10-10 11:54:25 -0300

    Imperator algorithms base structure

---
+++ luis_silva/src_ordenacao_alianca_rebelde_algoritmo2/main.cpp
@@ -0,0 +1,16 @@
+/*
+O método escolhido para o algorítmo 2 do imperador
+foi o Insertion Sort
+*/
+
+#include <cstdio>
+#include <iostream>
+#include "headers/civilizacao.hpp"
+#include "headers/insertion.hpp"
+
+int main(){
+
+    int n_linhas;
+
+    return 0;
+}
---
+++ luis_silva/src_ordenacao_imperador_algoritmo1/main.cpp
@@ -0,0 +1,4 @@
+/*
+O método escolhido para o algorítmo 1 do imperador
+foi o Selection Sort
+*/
